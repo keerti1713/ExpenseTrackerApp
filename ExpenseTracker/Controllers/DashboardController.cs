a57d18ed2045b0207369fbc9926ac5603b4a0990
Keerti Mishra <mishrakeerti1713@gmail.com>
2024-08-23 01:32:20 +0530

    Commit with project setup

---
+++ ExpenseTracker/Controllers/DashboardController.cs
@@ -0,0 +1,61 @@
+﻿using ExpenseTracker.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System;
+
+namespace ExpenseTracker.Controllers
+{
+    public class DashboardController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public DashboardController(ApplicationDbContext context) { 
+            _context = context;
+        }
+        public async Task<ActionResult> Index()
+        {
+            DateTime start = DateTime.Today.AddDays(-30);
+            DateTime end = DateTime.Today;
+            List<Transaction> SelectedTransactions= await _context.Transactions
+                .Include(x => x.Category)
+                .Where(y => y.Date >= start && y.Date <= end)
+                .ToListAsync();
+
+            int TotalIncome = SelectedTransactions
+                .Where(i => i.Category.Type == "Income")
+                .Sum(j => j.Amount);
+            ViewBag.TotalIncome = TotalIncome.ToString("C0");
+
+            int TotalExpense = SelectedTransactions
+                .Where(i => i.Category.Type == "Expense")
+                .Sum(j => j.Amount);
+            ViewBag.TotalExpense = TotalExpense.ToString("C0");
+
+            int Balance = TotalIncome-TotalExpense;
+            ViewBag.Balance = Balance.ToString("C0");
+
+            if (TotalExpense > TotalIncome)
+            {
+                TotalExpense = TotalIncome; 
+            }
+
+            var chartData = new
+            {
+                labels = new[] { "Income", "Expense", "Balance" },
+                data = new[] { TotalIncome, TotalExpense, Balance }
+            };
+            ViewBag.ChartData = chartData;
+
+            ViewBag.RecentTransactions = await _context.Transactions
+                .Include(i => i.Category)
+                .OrderByDescending(j => j.Date)
+                .Take(10)
+                .ToListAsync();
+
+
+            return View();
+        }
+    }
+}
---
+++ ExpenseTracker/Controllers/TransactionController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ExpenseTracker.Models;
+
+namespace ExpenseTracker.Controllers
+{
+    public class TransactionController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public TransactionController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Transactions.Include(t => t.Category);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var transaction = await _context.Transactions
+                .Include(t => t.Category)
+                .FirstOrDefaultAsync(m => m.TransactionId == id);
+            if (transaction == null)
+            {
+                return NotFound();
+            }
+
+            return View(transaction);
+        }
+
+        public IActionResult AddOrEdit(int id=0)
+
+        {
+            PopulateCategories();
+            if (id == 0)
+            {
+                return View(new Transaction());
+            }
+            else
+            {
+                return View(_context.Transactions.Find(id));
+            }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> AddOrEdit([Bind("TransactionId,CategoryId,Amount,Note,Date")] Transaction transaction)
+        {
+            if (ModelState.IsValid)
+            {
+                if (transaction.TransactionId == 0)
+                {
+                    _context.Add(transaction);
+                }
+                else
+                {
+                    _context.Update(transaction);
+                }
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            PopulateCategories();
+            return View(transaction);
+        }
+
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var transaction = await _context.Transactions
+                .Include(t => t.Category)
+                .FirstOrDefaultAsync(m => m.TransactionId == id);
+            if (transaction == null)
+            {
+                return NotFound();
+            }
+
+            return View(transaction);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var transaction = await _context.Transactions.FindAsync(id);
+            if (transaction != null)
+            {
+                _context.Transactions.Remove(transaction);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        [NonAction]
+        public void PopulateCategories()
+        {
+            var categoryCollection = _context.Categories.ToList();
+            var defaultCategory = new Category { CategoryId = 0, Title = "Choose a Category" };
+            categoryCollection.Insert(0, defaultCategory);
+
+            ViewBag.Categories = new SelectList(categoryCollection, "CategoryId", "Title");
+
+        }
+        private bool TransactionExists(int id)
+        {
+            return _context.Transactions.Any(e => e.TransactionId == id);
+        }
+    }
+}
